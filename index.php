cf029eb352df7d24722c23e855d09b01cd01dd0b
FedericoCattani <federico.cattani1@gmail.com>
2023-05-18 16:38:16 +0200

    to do list ripresa

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!--FontAwesome-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+  <!--Vue-->
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+  <!--Axios-->
+  <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
+
+  <!--CSS-->
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>PHP ToDo List JSON</title>
+</head>
+
+<body>
+
+  <div id="app">
+    <h1>ToDo List PHP</h1>
+    <!-- area di input -->
+    <div class="input-area">
+      <input v-model.trim="newTaskStrg" @keyup.enter="addNewTask" type="text" placeholder="New Task">
+      <button @click="addNewTask">Aggiungi</button>
+      <div>
+        <span class="error">{{errorMsg}}</span>
+      </div>
+
+    </div>
+
+    <!--lista-->
+    <div class="container">
+
+      <h2 v-if="tasks.length === 0">Non ci sono tasks! &#128526;</h2>
+
+      <ul v-else class="tasks-list">
+
+        <li v-for="(task, index) in tasks" :class="{'done' : task.done}" @click="task.done = !task.done">
+
+          <span>{{task.text}}</span>
+          <i @click.stop="removeTask(index)" class="fa-solid fa-trash-can"></i>
+        </li>
+
+      </ul>
+
+
+    </div>
+  </div>
+
+
+  <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,49 @@
+const { createApp } = Vue;
+
+createApp({
+
+  data() {
+    return {
+      tasks: [],
+      errorMsg: '',
+      newTaskStrg: '',
+    }
+  },
+
+  methods: {
+
+    removeTask(index) {
+      this.errorMsg = ''
+      if (this.tasks[index].done) {
+        this.tasks.splice(index, 1)
+      } else {
+        this.writeErrorMsg('Attenzione! Non puoi eliminare questa task se non è stata fatta.')
+      }
+
+    },
+
+    writeErrorMsg(msg) {
+      this.errorMsg = msg
+      setTimeout(() => {
+        this.errorMsg = ''
+      }, 2500)
+
+    },
+
+    addNewTask() {
+
+      if (this.newTaskStrg.length > 4) {
+        const newTask = {
+          text: this.newTaskStrg,
+          done: false
+        }
+        this.tasks.unshift(newTask),
+          this.newTaskStrg = ''
+      } else {
+        this.writeErrorMsg('Attenzione! La task deve essere almeno di cinque caratteri.')
+      }
+    }
+
+  }
+
+}).mount('#app')
